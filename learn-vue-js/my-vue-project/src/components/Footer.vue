c0d55c139bce8534ad5e1c17ba1faca519abcd97
Rein <reinX244@gmail.com>
2026-02-08 23:09:47 +0800

    Added learning vue files.

---
+++ learn-vue-js/my-vue-project/src/components/Footer.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from "vue";
+const footerYear = ref(new Date().getFullYear()); // get the current year
+</script>
+
+<template>
+  <footer>
+    <p>&copy; {{ footerYear }}</p>
+  </footer>
+</template>
+
+<style scoped>
+footer {
+  color: blue;
+  position: fixed;
+  bottom: 0;
+  padding-bottom: 20px;
+}
+</style>
