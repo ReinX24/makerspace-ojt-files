353447fcf9748c1af67e8db244f4db0e7dd0dade
Rein <reinX244@gmail.com>
2026-02-09 23:48:00 +0800

    Currently on vue crash course.

---
+++ learn-vue-js/vue-crash-2024/src/App.vue
@@ -0,0 +1,48 @@
+<script>
+import ref from "vue";
+
+export default {
+  setup() {
+    const name = "Rein Solis";
+    const status = "active";
+    const tasks = ["Task One", "Task Two", "Task Three"];
+
+    const toggleStatus = () => {
+      if (this.status === "active") {
+        this.status = "pending";
+      } else if (this.status === "pending") {
+        this.status = "inactive";
+      } else {
+        this.status = "active";
+      }
+    };
+
+    return {
+      name,
+      status,
+      tasks,
+      toggleStatus,
+    };
+  },
+};
+</script>
+
+<template>
+  <div>
+    <h1>{{ name }}</h1>
+    <p v-if="status === 'active'">User is active</p>
+    <p v-else-if="status === 'pending'">User is pending</p>
+    <p v-else>User is inactive</p>
+
+    <h3>Tasks:</h3>
+    <ul>
+      <li v-for="task in tasks" :key="task">{{ task }}</li>
+    </ul>
+    <!-- <a v-bind:href="link" target="_blank">Click for Google</a> -->
+    <a :href="link" target="_blank">Click for Google</a>
+    <br />
+    <button @click="toggleStatus">Change Status</button>
+  </div>
+</template>
+
+<style scoped></style>
