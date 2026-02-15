4ced06d9c92cf14a2d91bf474082c11fe681133b
Rein <reinX244@gmail.com>
2026-02-15 23:03:46 +0800

    Removed unneeded files and kept files for current crash course.

--- learn-vue-js/vue-crash-2024/src/App.vue
+++ learn-vue-js/vue-crash-2024/src/App.vue
@@ -1,30 +1,48 @@
-<script>
-import ref from "vue";
+<script setup>
+import { ref, onMounted } from "vue";
 
-export default {
-  setup() {
-    const name = "Rein Solis";
-    const status = "active";
-    const tasks = ["Task One", "Task Two", "Task Three"];
+const name = ref("Rein Solis");
+const status = ref("active");
+const tasks = ref(["Task One", "Task Two", "Task Three"]);
+const link = ref("https://www.google.com");
+const newTask = ref("");
 
-    const toggleStatus = () => {
-      if (this.status === "active") {
-        this.status = "pending";
-      } else if (this.status === "pending") {
-        this.status = "inactive";
-      } else {
-        this.status = "active";
-      }
-    };
-
-    return {
-      name,
-      status,
-      tasks,
-      toggleStatus,
-    };
-  },
+const toggleStatus = () => {
+  if (status.value === "active") {
+    status.value = "pending";
+  } else if (status.value === "pending") {
+    status.value = "inactive";
+  } else {
+    status.value = "active";
+  }
 };
+
+const addTask = () => {
+  if (newTask.value.trim() !== "") {
+    // Adding the newTask to our array
+    tasks.value.push(newTask.value);
+    // Clearing the newTask value
+    newTask.value = "";
+  }
+};
+
+const deleteTask = (index) => {
+  // Removing the element from our array
+  tasks.value.splice(index, 1);
+};
+
+// This runs as the page is loaded
+onMounted(async () => {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/todos");
+    const data = await response.json();
+    tasks.value = data.map((task) => {
+      return task.title;
+    });
+  } catch (error) {
+    console.error("Error fetching tasks");
+  }
+});
 </script>
 
 <template>
@@ -34,10 +52,25 @@ export default {
     <p v-else-if="status === 'pending'">User is pending</p>
     <p v-else>User is inactive</p>
 
+    <form @submit.prevent="addTask">
+      <label for="newTask">Add Task</label>
+      <input type="text" id="newTask" name="newTask" v-model="newTask" />
+
+      <button type="submit">Submit</button>
+    </form>
+
     <h3>Tasks:</h3>
     <ul>
-      <li v-for="task in tasks" :key="task">{{ task }}</li>
+      <li v-for="(task, index) in tasks" :key="task">
+        <span>
+          {{ task }}
+        </span>
+
+        <!-- Delete button for the task -->
+        <button @click="deleteTask(index)">x</button>
+      </li>
     </ul>
+
     <!-- <a v-bind:href="link" target="_blank">Click for Google</a> -->
     <a :href="link" target="_blank">Click for Google</a>
     <br />
