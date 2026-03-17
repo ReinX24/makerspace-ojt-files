c8faa2511e3cfa8f5ac5c4f3f4d3c4b3eb732d87
Rein <reinX244@gmail.com>
2026-03-17 16:14:06 +0800

    Started dev-event project.

--- nextjs_16_jsmastery/nextjs_demo/app/(root)/page.tsx
+++ nextjs_16_jsmastery/nextjs_demo/app/(root)/page.tsx
@@ -1,13 +1,22 @@
-import Hello from "@/components/Hello";
+const Home = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/albums");
 
-const Home = () => {
-  console.log("What type of component am I?");
+  if (!response.ok) throw new Error("Failed to fetch data");
+
+  const albums = await response.json();
 
   return (
-    <main>
-      <div className="text-5xl underline">Welcome to Next.js!</div>
-      <Hello />
-    </main>
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols">
+      {albums.map((album: { id: number; title: string }) => (
+        <div
+          key={album.id}
+          className="bg-white shadow-md rounded-lg p-4 transition"
+        >
+          <h3 className="text-lg font-bold mb-2">{album.title}</h3>
+          <p className="text-gray-600">Album ID: {album.title}</p>
+        </div>
+      ))}
+    </div>
   );
 };
 
---
+++ nextjs_16_jsmastery/nextjs_demo/app/api/db.ts
@@ -0,0 +1,9 @@
+// app/api/db.ts
+
+const books = [
+  { id: 1, name: "Atomic Habits" },
+  { id: 2, name: "Deep Work" },
+  { id: 3, name: "Norwegian Wood" },
+];
+
+export default books;
